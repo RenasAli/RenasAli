55e80515eef49daf95fa5bb828ca86cf35666a2a
RenasAli <rena0506@stud.kea.dk>
2021-09-29 09:35:40 +0200

    Add git project

--- src/GitProjekt.java
+++ src/GitProjekt.java
@@ -2,5 +2,7 @@ public class GitProjekt {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("test");
+        System.out.println("test2");
+
     }
 }
